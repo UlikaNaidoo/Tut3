9816297b8112f6318bd2ee315604bfcfe12db2f3
UlikaNaidoo <ulika.d.naidoo@gmail.com>
2016-03-21 17:59:20 +0200

    Random Generator Working

--- Dice- T3/Source.cpp
+++ Dice- T3/Source.cpp
@@ -3,47 +3,47 @@
 #include <ctime>
 using namespace std;
 
-void average(float a)
-{
-		cout << "Average of Dice Rolls" << a << endl;
-}
+//void average(float a)
+//{
+	//	cout << "Average of Dice Rolls" << a << endl;
+//}
+//void average(float b)
+//{
+	//cout << "Average of array" << b << endl;
+//}
 
 
 int main()
 {
-	int x;
-	int y;
-	float z;
-	int v;
-	
-	//cout << "Enter Number of Rolls of Dice" << endl;
-	//cin >> x;
-	//cout << "Enter index of Array" << endl;
-	//cin >> y;
-	//cout << endl;
+	int a;
+	int z = 0;
 
-	x = 6;
 	srand(time(0)); // Using number of seconds function to ensure random number
-	for (int p = 1; p = x + 1; p++) // From 1 to user input # of rolls adn incremented by 1
-	{ 
-		// z += 0;
-		cout << 1 + rand() % 6 << endl; // Using Rand() function to generate numbers randomly
-	}
-	v = x / z;
-	average(v);
-
-	int array[5] = { 0, 1, 2, 3, 4 };
-	int sum = 0;
-	int s;
-
-	for (int v = 0; v < 5; v++)
+	for (int p = 1; p < 5; p++) // From 1 to user input # of rolls and incremented by 1 to ensure random # not 0
 	{
-		sum += array[v]; 
-		s = sum / 5;
-		cout << "Average of Array is" << s << endl;
-	}
+		a = 1 + rand() % 6;
+		cout << a << endl; // Using Rand() function to generate numbers randomly
+		z = z + a; // Calculating sum of random numbers
 
-	system("pause"); 
+	}
+	cout << "The Sum is " << z << endl;
+	//v = x / z;
+	//average(v);
+
+
+	//int array[5] = { 0, 1, 2, 3, 4 };
+	//int sum = 0;
+	//int s;
+
+	//for (int v = 0; v < 5; v++)
+	//{
+	//sum += array[v]; 
+	//s = sum / 5;
+	//average(s);
+
+	//}
+	system("pause");
+	return 0;
 }
 
 
